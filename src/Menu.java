5296a632f3cde46709cb559e17c2f5fb089c3273
Roman Shkurdyuk <romansh0106@gmail.com>
2025-10-18 11:22:52 +0300

    feat: Implement README.md

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Comparator;
 
